c1b110e8dde68e3c8496f4710baf09dc6100dc57
merajhussainn <mh7803255@gmail.com>
2026-08-13 14:32:05 +0530

    Dashboard Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
-import Home from './pages/Home'
+import Home from './pages/Dashboard'
 import PlaceholderPage from './pages/PlaceholderPage'
 
 function App() {
